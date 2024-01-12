f1f9364e2ef2e05325d79014d4be2422e84bc185
negyeliczkysandor <sandor.negyeliczky@gmail.com>
2024-01-12 23:23:38 +0100

    add: antDesign

--- documentation/APPROACH.md
+++ documentation/APPROACH.md
@@ -7,6 +7,9 @@ Use this section to write down your intended plan for the test.
 
 ## Comments Whilst Developing
 
+1. I spent time to understand the  spaceX API because it was new to me, than used postman to find the right endpoints for my needs. Than implemented in the api and point. I also spend some time to understand the RTK Query with the linked tutorial 
+2. Find a typesript and styled component friendly ui library AntDesign. I used it in my previous project, but it has very good documentation easy to use and fits good with React.
+
 <!--
 Use this section to not down anything whilst developing e.g. challenging bits of work, reasons for a certain approach, reason for adding a specific package etc
 -->
--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import {
   useLaunchesForPayloadsQuery,
   usePayloadsNationalitiesQuery,
 } from "./services/spaceXApi";
+import { Button, Dropdown, Space, message } from "antd";
+import { DownOutlined, UserOutlined } from "@ant-design/icons";
+import type { MenuProps } from "antd";
 
 function App() {
   const { data } = usePayloadsNationalitiesQuery();
@@ -26,9 +29,53 @@ function App() {
 
   console.log(uniqueNations);
 
+  const handleMenuClick: MenuProps["onClick"] = (e) => {
+    message.info("Click on menu item.");
+    console.log("click", e);
+  };
+
+  const items: MenuProps["items"] = [
+    {
+      label: "1st menu item",
+      key: "1",
+      icon: <UserOutlined />,
+    },
+    {
+      label: "2nd menu item",
+      key: "2",
+      icon: <UserOutlined />,
+    },
+    {
+      label: "3rd menu item",
+      key: "3",
+      icon: <UserOutlined />,
+      danger: true,
+    },
+    {
+      label: "4rd menu item",
+      key: "4",
+      icon: <UserOutlined />,
+      danger: true,
+      disabled: true,
+    },
+  ];
+
+  const menuProps = {
+    items,
+    onClick: handleMenuClick,
+  };
+
   return (
     <div className="app">
       <img src={logo} className="logo" alt="Vite logo" />
+      <Dropdown menu={menuProps}>
+        <Button>
+          <Space>
+            Button
+            <DownOutlined />
+          </Space>
+        </Button>
+      </Dropdown>
       <header className="header">
         <h2>Welcome to the Frontend Technical Test</h2>
         <p>Please check the README before you start.</p>
